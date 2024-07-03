4e868140ed4378898ab0feac9ce4591f27d40dc1
raphskal <raphael.kalender@gmail.com>
2024-07-03 12:50:24 +0200

    replace postmark token with secret

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,40 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const postmark = require('postmark');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 const cors = require('cors')({ origin: true });
 
 admin.initializeApp();
 
-// Replace with your Postmark API key
-const POSTMARK_API_KEY = 'eb2e845d-2771-43bc-925f-a4e1085d0afd';
-const client = new postmark.ServerClient(POSTMARK_API_KEY);
+const client = new SecretManagerServiceClient();
+
+async function getPostmarkApiKey() {
+    const [version] = await client.accessSecretVersion({
+        name: 'projects/1015569732566/secrets/POSTMARK_API_KEY/versions/latest',
+    });
+
+    const payload = version.payload.data.toString('utf8');
+    console.log(`Secret data: ${payload}`)
+    return payload;
+}
 
 exports.sendEmail = functions.https.onRequest((req, res) => {
     cors(req, res, async () => {
         const { email, question } = req.body;
 
-        const emailOptions = {
-            "From": "raphael@raphskal.de", // Use your verified Postmark email
-            "To": email,
-            "Subject": "Danke für Ihre Anfrage.",
-            "TextBody": `Wir haben folgende Anfrage von Ihnen erhalten:\n ${question}\n\n
-            Wir werden uns zeitnah bei Ihnen melden.`,
-        };
-
         try {
-            await client.sendEmail(emailOptions);
+            const POSTMARK_API_KEY = await getPostmarkApiKey();
+            const postmarkClient = new postmark.ServerClient(POSTMARK_API_KEY);
+
+            const emailOptions = {
+                "From": "raphael@raphskal.de", // Use your verified Postmark email
+                "To": email,
+                "Subject": "Danke für Ihre Anfrage.",
+                "TextBody": `Wir haben folgenden Anfrage von Ihnen erhalten:\n ${question}\n\n
+                Wir melden uns bei Ihnen zeitnah.`,
+            };
+
+            await postmarkClient.sendEmail(emailOptions);
             res.status(200).send({ success: true, message: 'Email sent successfully' });
         } catch (error) {
             console.error(error);
